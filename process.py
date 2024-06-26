233116b84c5dbc8eb5e5dcba60d46eb787cce44d
vuongminh1907 <vuongminhkhoi4@gmail.com>
2024-06-27 00:53:57 +0700

    .

---
+++ process.py
@@ -0,0 +1,204 @@
+from network import U2NET
+
+import os
+from PIL import Image
+import cv2
+import gdown
+import argparse
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+
+from collections import OrderedDict
+from options import opt
+
+
+def load_checkpoint(model, checkpoint_path):
+    if not os.path.exists(checkpoint_path):
+        print("----No checkpoints at given path----")
+        return
+    model_state_dict = torch.load(checkpoint_path, map_location=torch.device("cpu"))
+    new_state_dict = OrderedDict()
+    for k, v in model_state_dict.items():
+        name = k[7:]  # remove `module.`
+        new_state_dict[name] = v
+
+    model.load_state_dict(new_state_dict)
+    print("----checkpoints loaded from path: {}----".format(checkpoint_path))
+    return model
+
+
+def get_palette(num_cls):
+    """ Returns the color map for visualizing the segmentation mask.
+    Args:
+        num_cls: Number of classes
+    Returns:
+        The color map
+    """
+    n = num_cls
+    palette = [0] * (n * 3)
+    for j in range(0, n):
+        lab = j
+        palette[j * 3 + 0] = 0
+        palette[j * 3 + 1] = 0
+        palette[j * 3 + 2] = 0
+        i = 0
+        while lab:
+            palette[j * 3 + 0] |= (((lab >> 0) & 1) << (7 - i))
+            palette[j * 3 + 1] |= (((lab >> 1) & 1) << (7 - i))
+            palette[j * 3 + 2] |= (((lab >> 2) & 1) << (7 - i))
+            i += 1
+            lab >>= 3
+    return palette
+
+
+class Normalize_image(object):
+    """Normalize given tensor into given mean and standard dev
+
+    Args:
+        mean (float): Desired mean to substract from tensors
+        std (float): Desired std to divide from tensors
+    """
+
+    def __init__(self, mean, std):
+        assert isinstance(mean, (float))
+        if isinstance(mean, float):
+            self.mean = mean
+
+        if isinstance(std, float):
+            self.std = std
+
+        self.normalize_1 = transforms.Normalize(self.mean, self.std)
+        self.normalize_3 = transforms.Normalize([self.mean] * 3, [self.std] * 3)
+        self.normalize_18 = transforms.Normalize([self.mean] * 18, [self.std] * 18)
+
+    def __call__(self, image_tensor):
+        if image_tensor.shape[0] == 1:
+            return self.normalize_1(image_tensor)
+
+        elif image_tensor.shape[0] == 3:
+            return self.normalize_3(image_tensor)
+
+        elif image_tensor.shape[0] == 18:
+            return self.normalize_18(image_tensor)
+
+        else:
+            assert "Please set proper channels! Normlization implemented only for 1, 3 and 18"
+
+
+
+
+def apply_transform(img):
+    transforms_list = []
+    transforms_list += [transforms.ToTensor()]
+    transforms_list += [Normalize_image(0.5, 0.5)]
+    transform_rgb = transforms.Compose(transforms_list)
+    return transform_rgb(img)
+
+
+
+def generate_mask(input_image, net, palette, device = 'cpu',position = 'Upper body'):
+
+    #img = Image.open(input_image).convert('RGB')
+    img = input_image
+    img_size = img.size
+    img = img.resize((768, 768), Image.BICUBIC)
+    scale = [768 / img_size[0], 768 / img_size[1]]
+    image_tensor = apply_transform(img)
+    image_tensor = torch.unsqueeze(image_tensor, 0)
+
+    alpha_out_dir = os.path.join(opt.output,'alpha')
+    cloth_seg_out_dir = os.path.join(opt.output,'cloth_seg')
+
+    os.makedirs(alpha_out_dir, exist_ok=True)
+    os.makedirs(cloth_seg_out_dir, exist_ok=True)
+
+    with torch.no_grad():
+        output_tensor = net(image_tensor.to(device))
+        output_tensor = F.log_softmax(output_tensor[0], dim=1)
+        output_tensor = torch.max(output_tensor, dim=1, keepdim=True)[1]
+        output_tensor = torch.squeeze(output_tensor, dim=0)
+        output_arr = output_tensor.cpu().numpy()
+    mask_image = Image.fromarray(output_arr[0].astype(np.uint8), mode='P')
+    mask_image_cloth = mask_image.copy()
+    #Cut the image according to the position
+    if position == 'Upper body':
+        pixels = np.where(output_arr == 1)
+        alpha_mask = (output_arr == 1).astype(np.uint8) * 255
+        alpha_mask = alpha_mask[0]  # Selecting the first channel to make it 2D
+        alpha_mask_img = Image.fromarray(alpha_mask, mode='L')
+    elif position == 'Lower body':
+        pixels = np.where(output_arr == 2)
+        alpha_mask = (output_arr == 2).astype(np.uint8) * 255
+        alpha_mask = alpha_mask[0]  # Selecting the first channel to make it 2D
+        alpha_mask_img = Image.fromarray(alpha_mask, mode='L')
+    elif position == 'Full body':
+        pixels = np.where(output_arr == 3)
+        alpha_mask = (output_arr == 3).astype(np.uint8) * 255
+        alpha_mask = alpha_mask[0]  # Selecting the first channel to make it 2D
+        alpha_mask_img = Image.fromarray(alpha_mask, mode='L')
+
+    x_min, x_max = np.min(pixels[1]), np.max(pixels[1])
+    y_min, y_max = np.min(pixels[2]), np.max(pixels[2])
+    mask_image = img.crop((y_min, x_min, y_max, x_max))
+    mask_image_size = mask_image.size
+    mask_image_size = (mask_image_size[0] / scale[0], mask_image_size[1] / scale[1])
+    mask_image = mask_image.resize((int(mask_image_size[0]), int(mask_image_size[1])), Image.BICUBIC)
+    #mask_image = mask_image.resize(img_size, Image.BICUBIC)
+    mask_image.save(os.path.join(cloth_seg_out_dir, 'mask_new.png'))
+
+    white_background = Image.new('RGB', img.size, (255, 255, 255))
+    masked_img = Image.composite(img, white_background, alpha_mask_img)
+    masked_img = masked_img.crop((y_min, x_min, y_max, x_max))
+    masked_img.save(os.path.join(cloth_seg_out_dir, 'masked_img.png'))
+    
+    return masked_img
+
+
+
+def check_or_download_model(file_path):
+    if not os.path.exists(file_path):
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        url = "https://drive.google.com/uc?id=11xTBALOeUkyuaK3l60CpkYHLTmv7k3dY"
+        gdown.download(url, file_path, quiet=False)
+        print("Model downloaded successfully.")
+    else:
+        print("Model already exists.")
+
+
+def load_seg_model(checkpoint_path, device='cpu'):
+    net = U2NET(in_ch=3, out_ch=4)
+    check_or_download_model(checkpoint_path)
+    net = load_checkpoint(net, checkpoint_path)
+    net = net.to(device)
+    net = net.eval()
+
+    return net
+
+
+def main(args):
+
+    device = 'cuda:0' if args.cuda else 'cpu'
+
+    # Create an instance of your model
+    model = load_seg_model(args.checkpoint_path, device=device)
+
+    palette = get_palette(4)
+
+    img = Image.open(args.image).convert('RGB')
+    position = args.position
+    cloth_seg = generate_mask(img, net=model, palette=palette, device=device, position=position)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Help to set arguments for Cloth Segmentation.')
+    parser.add_argument('--image', type=str, help='Path to the input image')
+    parser.add_argument('--cuda', action='store_true', help='Enable CUDA (default: False)')
+    parser.add_argument('--checkpoint_path', type=str, default='model/cloth_segm.pth', help='Path to the checkpoint file')
+    parser.add_argument('--position', type=str, default='Upper body', help='Position to segment (Upper body, Lower body, Full body)')
+    args = parser.parse_args()
+
+    main(args)
